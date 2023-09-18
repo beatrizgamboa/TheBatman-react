c8382c20393ffe9c36fe75b12a982b9e43cd0be6
Beatriz Gambôa <biabgamboa@gmail.com>
2023-09-18 08:33:44 -0300

    First Commit

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import {  BrowserRouter, Routes, Route } from 'react-router-dom'
 function App() {
  
   return (
-    <BrowserRouter>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/contato' element={<Contato />} />
